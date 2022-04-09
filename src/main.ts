da910b8fe1c182ef0cf2f13ed34037f688aca2fc
Lance Turbes <lturbes@mahalobanking.com>
2022-04-09 13:41:32 -0700

    feat: add basic animated/lit scene

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,64 @@
 import {
+    MathUtils,
     Scene,
     PerspectiveCamera,
     WebGLRenderer,
     BoxGeometry,
+    MeshStandardMaterial,
     MeshBasicMaterial,
     Mesh,
+    PointLight,
+    PlaneGeometry,
 } from "three";
 
-const scene = new Scene();
-const camera = new PerspectiveCamera(
-    85,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-);
+// Helpers
+const deg = (angle: number): number => MathUtils.degToRad(angle);
 
-// Create Renderer
-const renderer = new WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+// Set Up Renderer
+const WIDTH = innerWidth;
+const HEIGHT = innerHeight;
+const renderer = new WebGLRenderer({ antialias: true });
+renderer.setSize(WIDTH, HEIGHT);
+renderer.setClearColor("grey");
+
+// Mount the Renderer
+const app = document.getElementById("app")!;
+app.appendChild(renderer.domElement);
+
+// Create Camera
+const camera = new PerspectiveCamera(70, WIDTH / HEIGHT);
+camera.position.set(0, 5, 10);
+camera.rotation.x = deg(-25);
+
+// Create Plane
+const planeGeo = new PlaneGeometry(10, 10);
+const planeMat = new MeshStandardMaterial({ color: "white" });
+const plane = new Mesh(planeGeo, planeMat);
+plane.position.y = -1;
+plane.rotation.x = deg(-90);
 
 // Create Cube
-const geometry = new BoxGeometry();
-const material = new MeshBasicMaterial({ color: "orange" });
-const cube = new Mesh(geometry, material);
+const cubeGeo = new BoxGeometry();
+const cubeMat = new MeshStandardMaterial({ color: "blue" });
+const cube = new Mesh(cubeGeo, cubeMat);
+cube.position.y = 0.5;
+cube.rotation.set(0.2, 0.5, 0);
+
+// Create Light
+const light = new PointLight("white");
+light.position.set(2, 3, 2);
+
+// Create Scene
+const scene = new Scene();
+scene.add(plane);
 scene.add(cube);
+scene.add(light);
 
-// Adjust Camera Position
-camera.position.z = 5;
-
-// Animation Loop
-function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+// Loop
+function update() {
+    requestAnimationFrame(update);
+    cube.rotateY(0.005);
+    cube.rotateZ(0.005);
     renderer.render(scene, camera);
 }
-animate();
+update();
